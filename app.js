26d61086dbf76fb3dc446644dfddb2a11f6914eb
Evan Vujcec <evanvujcec@gmail.com>
2018-10-27 14:55:49 -0400

    Basic API connectors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var organizationRouter = require('./routes/organization');
+var eventRouter = require('./routes/event');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/organization', organizationRouter);
+app.use('/event', eventRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/event.js
+++ routes/event.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var controller = require('../controllers/event')
 var router = express.Router();
 
 /* GET users listing. */
 
 // Get user by ID
 router.get('/', function (req, res, next) {
-  res.send(`Name: ${req.query['username']}`);
+  res.send(`Event ID: ${req.query['id']}`);
 });
 
 module.exports = router;
